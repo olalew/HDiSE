adc97300944b089ae2e80e21055c4bb72d631d60
sudokit <ola.lewandowska12@gmail.com>
2023-07-09 18:58:04 +0200

    fixed

--- logging-scripts/admin-period/run.sh
+++ logging-scripts/admin-period/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd /home/sudokit/Documents/College/Engineering/S6/HDiSE/HDiSE/logging-scripts/admin-period
+
 source ./scan_process_list.sh
 source ./read_log_messages.sh
--- logging-scripts/docker-periodical/run.sh
+++ logging-scripts/docker-periodical/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/sudokit/Documents/College/Engineering/S6/HDiSE/HDiSE/logging-scripts/docker-periodical
+
 source ./docker/read_images.sh
 source ./docker/read_docker_containers.sh
 source ./docker/read_logs.sh
